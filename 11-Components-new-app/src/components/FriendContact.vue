b6276ee247cb7cd90fb18d27e2f6a3d676898842
Vimlesh-Kumar <vimlesh11072000@gmail.com>
2023-03-23 18:21:14 +0530

    friends-contact-app using components

---
+++ 11-Components-new-app/src/components/FriendContact.vue
@@ -0,0 +1,61 @@
+<template>
+  <li>
+    <h2>{{ name }} {{ isFavourite ? '(Favourite)' : '' }}</h2>
+    <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+    <button @click="toggleFavourite">Favourite</button>
+    <ul v-if="detailsAreVisible">
+      <li>
+        <strong>Phone:</strong>
+        {{ phoneNumber }}
+      </li>
+      <li>
+        <strong>Email:</strong>
+        {{ emailAddress }}
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  // props: ['name','phoneNumber','emailAddress','isFavourite'],
+  props:{
+    id: {
+      type: String,
+      required: true
+    },
+    name:{
+      type:String,
+      required:true
+    },
+    phoneNumber: {
+      type: String,
+      required: true
+    },
+    emailAddress: {
+      type: String,
+      required: true
+    },
+    isFavourite:{
+      type:Boolean,
+      required:true
+      
+    }
+
+  },
+  data() {
+    return {
+      detailsAreVisible: false,
+      favouriteFriend: this.isFavourite,
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavourite() {
+      this.$emit('toggle-favourite', this.id)
+    }
+  }
+};
+</script>
