e8563f94b168809a737c58033e70abef568b6fc4
Vimlesh-Kumar <vimlesh11072000@gmail.com>
2023-03-27 18:45:10 +0530

    forms,http&routing

---
+++ 18-http-requests/src/components/survey/UserExperiences.vue
@@ -0,0 +1,69 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Submitted Experiences</h2>
+      <div>
+        <base-button @click="loadExperience">Load Submitted Experiences</base-button>
+      </div>
+      <p v-if="isLoading">Loading...</p>
+      <p v-else-if="!isLoading && error">{{ error }}</p>
+      <p v-else-if="!isLoading &&(!results || results.length===0)">No experience survey is found. Please add a survey first.</p>
+      <ul v-else>
+        <survey-result v-for="result in results" :key="result.id" :name="result.name"
+          :rating="result.rating"></survey-result>
+      </ul>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import SurveyResult from './SurveyResult.vue';
+
+export default {
+  components: {
+    SurveyResult,
+  },
+  data() {
+    return {
+      results: [],
+      isLoading:false,
+      error:null
+    }
+  },
+  methods: {
+    loadExperience() {
+      this.isLoading=true;
+      this.error=null;
+      fetch('https://vue-http-requests-c7ab5-default-rtdb.firebaseio.com/surveys.json').then(
+        (response) => {
+          if (response.ok) {
+            return response.json();
+          }
+        }
+      ).then((data) => {
+        this.isLoading=false;
+        const newResults = [];
+        for (const id in data) {
+          newResults.push({ id: id, name: data[id].name, rating: data[id].rating });
+        }
+        this.results = newResults
+      }).catch((error)=>{
+        console.log(error)
+        this.isLoading=false
+        this.error='Failed to fetch data - please try again later.'
+      });
+    }
+  },
+  mounted(){
+    this.loadExperience();
+  }
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
